f6cb216c3d0daf32fea146a12526673f4d825e98
Milan Jaric <327155+mjaric@users.noreply.github.com>
2021-09-17 11:09:38 +0200

    neko se predomislio :)

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import * as path from 'path';
+import { I18nModule, I18nJsonParser } from 'nestjs-i18n';
 
 @Module({
-  imports: [],
+  imports: [
+    I18nModule.forRoot({
+      fallbackLanguage: 'en',
+      parser: I18nJsonParser,
+      parserOptions: {
+        path: path.join(__dirname, '/i18n/'),
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
